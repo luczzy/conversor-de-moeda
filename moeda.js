ce71964b7863e59dd0f77a7dcbdf9ca9b78438c4
Deivid Lucas <deivid.marques@alunoescolassociais.org.br>
2024-10-24 09:53:44 -0300

    obtendo valores de entrada do formulario

--- moeda.js
+++ moeda.js
@@ -1,9 +1,8 @@
 document.getElementById('currency-form').addEventListener('submit', function (event){
 
 // obter valores de entrada formulário
-
-
-
-
+const valor = parseFloat(document.getElementById('amount').value);
+const moeda = document.getElementById('moeda').value;
+const moeda1 = document.getElementById('moeda1').value;
 
 });
