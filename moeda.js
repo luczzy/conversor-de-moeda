187d1d737abc5800b258fd230c97db537a89da31
Deivid Lucas <deivid.marques@alunoescolassociais.org.br>
2024-10-24 10:09:10 -0300

    adicionando estrutura que define taxa de cambio

--- moeda.js
+++ moeda.js
@@ -5,4 +5,10 @@ const valor = parseFloat(document.getElementById('amount').value);
 const moeda = document.getElementById('moeda').value;
 const moeda1 = document.getElementById('moeda1').value;
 
+// definir taxa de cambio fixa com utilização de objeto
+const exchangeRates = {
+    USD: {EUR: 0.93, BRL: 5.71},
+    BRL: {EUR: 0.16, USD: 0.18},
+    EUR: {EUR: 6.16, EUR: 1.08},
+};
 });
